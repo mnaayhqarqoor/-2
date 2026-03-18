4e04cd0742381a60cefc6a2ffa358f122696e288
mnaayhqarqoor <143949294+mnaayhqarqoor@users.noreply.github.com>
2026-03-18 17:43:35 +0300

    Delete code.js

--- code.js
+++
@@ -1,6 +0,0 @@
-function doPost(e) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  var data = JSON.parse(e.postData.contents);
-  sheet.appendRow([data.name, data.phone, "Participant"]);
-  return ContentService.createTextOutput("Success").setMimeType(ContentService.MimeType.TEXT);
-}
